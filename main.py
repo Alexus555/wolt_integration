7a06e484709a2d3f5657a04d7822bff3ffd59fbc
Alexus555 <alexus555@yandex.ru>
2022-06-17 15:30:28 +0600

    Logging added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import config
-
+import logging
+import os
+import sys
 
 def get_files_for_processing(data_path: str) -> list:
     pass
@@ -10,9 +12,23 @@ def process_file(file_name: str) -> None:
 
 
 if __name__ == '__main__':
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u"%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler(".\\script.log"),
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
+
+    logging.info("Start script")
+
     data_path = config.DATA_PATH
 
     processing_files = get_files_for_processing(data_path)
 
     for file_name in processing_files:
         process_file(file_name)
+
+    logging.info("Finish script")
